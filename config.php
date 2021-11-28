df38e46eba2e96e47d32f42c2456324dc9097cc5
phiphi1992 <phi.qtmax@gmail.com>
2021-11-28 23:44:57 +0700

    update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_PASSWORD = "1234";
-const DB_HOST = "123.123.123.123";
+const DB_PASSWORD = "hjR2Q3HvemryyNw3bZcF";
+const DB_HOST = "172.104.33.144";
 const DB_PORT = "3306";
 const DB_NAME = "webinar";
 const DB_USER = "root";
